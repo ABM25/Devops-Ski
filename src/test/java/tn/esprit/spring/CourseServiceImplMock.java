997c8bf38c070c5af7dd3f03e24ee47eb45326a0
SaneKarray <sane.karray@esprit.tn>
2023-11-07 11:50:23 +0100

    Updated test files and application.properties

--- src/test/java/tn/esprit/spring/CourseServiceImplMock.java
+++ src/test/java/tn/esprit/spring/CourseServiceImplMock.java
@@ -1,2 +1,72 @@
-package tn.esprit.spring;public class CourseServiceImplMock {
+package tn.esprit.spring;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Registration;
+import tn.esprit.spring.entities.Support;
+import tn.esprit.spring.entities.TypeCourse;
+import tn.esprit.spring.repositories.ICourseRepository;
+import tn.esprit.spring.services.CourseServicesImpl;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+public class CourseServiceImplMock {
+
+
+    @MockBean
+    ICourseRepository cr;
+
+    @InjectMocks
+    CourseServicesImpl cs;
+
+    Course course = new Course(Long.valueOf(1), 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, new Float(0), 1, new HashSet<Registration>());
+
+    List<Course> liste= new ArrayList<Course>(){
+        {
+            add(new Course(Long.valueOf(2),2, TypeCourse.COLLECTIVE_CHILDREN, Support.SNOWBOARD,new Float(0),2,null));
+            add(new Course(Long.valueOf(3),3, TypeCourse.COLLECTIVE_CHILDREN, Support.SNOWBOARD,new Float(0),3,null));
+
+        }
+
+    };
+
+
+
+
+    @Test
+    public void testRetriveCourse(){
+        Mockito.when(cr.findById(Mockito.anyLong())).thenReturn(Optional.of(course));
+        Course course1=cs.retrieveCourse(Long.valueOf(2));
+        Assertions.assertNotNull(course1);
+    }
+
+    @Test
+    public void testretrieveAllCourses(){
+        Mockito.when(cr.findAll()).thenReturn(liste);
+        List<Course> result=cs.retrieveAllCourses();
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void testupdateCourse(){
+        Course updatedCourse = new Course();
+        Mockito.when(cr.save(updatedCourse)).thenReturn(updatedCourse);
+        Course result=cs.updateCourse(updatedCourse);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void testaddCourse(){
+        Course course1 = new Course();
+        Mockito.when(cr.save(course1)).thenReturn(course1);
+        Course result=cs.updateCourse(course1);
+        Assertions.assertNotNull(result);
+    }
 }
--- src/test/java/tn/esprit/spring/CourseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/CourseServiceImplTest.java
@@ -1,2 +1,64 @@
-package tn.esprit.spring;public class CourseServiceImplTest {
+package tn.esprit.spring;
+
+
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Registration;
+import tn.esprit.spring.entities.Support;
+import tn.esprit.spring.entities.TypeCourse;
+import tn.esprit.spring.repositories.ICourseRepository;
+import tn.esprit.spring.services.CourseServicesImpl;
+import tn.esprit.spring.services.ICourseServices;
+
+import java.util.HashSet;
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CourseServiceImplTest {
+
+    @Autowired
+    private ICourseRepository cr;
+    @Autowired
+    ICourseServices cs;
+
+    Course c=new Course(Long.valueOf(1), 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, new Float(0), 1, new HashSet<Registration>());
+
+    @AfterEach
+    public void tearDown() {
+        // Clean up the database if needed
+        cr.deleteAll();
+    }
+
+    @Test
+    @Order(1)
+    public void testaddCourse(){
+        Course result=cs.addCourse(c);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    @Order(2)
+    public void testretrieveAllCourses(){
+        List<Course> result=cs.retrieveAllCourses();
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    @Order(3)
+    public void testupdateCourse(){
+        Course result=cs.updateCourse(c);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    @Order(4)
+    public void testretrieveCourse(){
+        c = cs.addCourse(c);
+        Course result=cs.retrieveCourse(c.getNumCourse());
+        Assertions.assertNotNull(result);
+    }
 }
