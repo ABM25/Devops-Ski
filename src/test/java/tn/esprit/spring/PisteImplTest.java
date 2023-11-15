f2ef3b848c237f9ae2c733a06bb3ceb7a9cddbbe
ABM25 <ahmed.benmami@esprit.tn>
2023-11-15 20:41:57 +0100

    2 fonctions seulementV2

--- src/test/java/tn/esprit/spring/PisteImplTest.java
+++ src/test/java/tn/esprit/spring/PisteImplTest.java
@@ -1,22 +1,22 @@
 package tn.esprit.spring;
 
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Piste;
-
-import java.util.List;
-import java.util.Optional;
-
 import tn.esprit.spring.repositories.IPisteRepository;
 import tn.esprit.spring.services.PisteServicesImpl;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PisteImplTest {
 
     @Mock
@@ -26,9 +26,8 @@ public class PisteImplTest {
     private PisteServicesImpl pisteServices;
 
     @Test
-
     void testAddPiste() {
-        // Create a sample subscription
+        // Create a sample piste
         Piste piste = new Piste();
         // Mock the behavior of the repository method
         when(pisteRepository.save(piste)).thenReturn(piste);
@@ -41,10 +40,7 @@ public class PisteImplTest {
         verify(pisteRepository, times(1)).save(piste);
     }
 
-
-
     @Test
-
     void testRetrievePiste() {
         Long numPiste = 1L;
         Piste piste = new Piste();
@@ -59,5 +55,4 @@ public class PisteImplTest {
         // Verify that findById method of pisteRepository was called once
         verify(pisteRepository, times(1)).findById(numPiste);
     }
-
 }
