ac1066647cf4558c9090143ac9237af7d61d9558
SaneKarray <sane.karray@esprit.tn>
2023-11-16 01:42:51 +0100

    promdddssseerttdsffdd

--- src/test/java/tn/esprit/spring/CourseServiceImplMock.java
+++ src/test/java/tn/esprit/spring/CourseServiceImplMock.java
@@ -1,61 +1,58 @@
 package tn.esprit.spring;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Registration;
 import tn.esprit.spring.entities.Support;
 import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.repositories.ICourseRepository;
 import tn.esprit.spring.services.CourseServicesImpl;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class CourseServiceImplMock {
 
-
-    @MockBean
+    @Mock
     ICourseRepository cr;
 
     @InjectMocks
     CourseServicesImpl cs;
 
-    Course course = new Course(Long.valueOf(1), 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, new Float(0), 1, new HashSet<Registration>());
+    Course course = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 0.0f, 1, new HashSet<>());
 
-    List<Course> liste= new ArrayList<Course>(){
+    List<Course> liste = new ArrayList<Course>() {
         {
-            add(new Course(Long.valueOf(2),2, TypeCourse.COLLECTIVE_CHILDREN, Support.SNOWBOARD,new Float(0),2,null));
-            add(new Course(Long.valueOf(3),3, TypeCourse.COLLECTIVE_CHILDREN, Support.SNOWBOARD,new Float(0),3,null));
-
+            add(new Course(2L, 2, TypeCourse.COLLECTIVE_CHILDREN, Support.SNOWBOARD, 0.0f, 2, null));
+            add(new Course(3L, 3, TypeCourse.COLLECTIVE_CHILDREN, Support.SNOWBOARD, 0.0f, 3, null));
         }
-
     };
 
-
-
-
-
-
-
     @Test
-    public void testupdateCourse(){
+    public void testupdateCourse() {
         Course updatedCourse = new Course();
         Mockito.when(cr.save(updatedCourse)).thenReturn(updatedCourse);
-        Course result=cs.updateCourse(updatedCourse);
-        Assertions.assertNotNull(result);
+        Course result = cs.updateCourse(updatedCourse);
+        assertNotNull(result);
     }
 
     @Test
-    public void testaddCourse(){
+    public void testaddCourse() {
         Course course1 = new Course();
         Mockito.when(cr.save(course1)).thenReturn(course1);
-        Course result=cs.updateCourse(course1);
-        Assertions.assertNotNull(result);
+        Course result = cs.addCourse(course1);
+        assertNotNull(result);
     }
 }
