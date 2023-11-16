e06798fc6d1657cd513f6e9d01dec3044660b4cf
SaneKarray <sane.karray@esprit.tn>
2023-11-16 10:12:03 +0100

    promdddssseerttdsffddssdesss

--- src/test/java/tn/esprit/spring/CourseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/CourseServiceImplTest.java
@@ -1,7 +1,5 @@
 package tn.esprit.spring;
 
-
-
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,11 +8,9 @@ import tn.esprit.spring.entities.Registration;
 import tn.esprit.spring.entities.Support;
 import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.repositories.ICourseRepository;
-import tn.esprit.spring.services.CourseServicesImpl;
 import tn.esprit.spring.services.ICourseServices;
 
 import java.util.HashSet;
-import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -22,10 +18,11 @@ public class CourseServiceImplTest {
 
     @Autowired
     private ICourseRepository cr;
-    @Autowired
-    ICourseServices cs;
 
-    Course c=new Course(Long.valueOf(1), 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, new Float(0), 1, new HashSet<Registration>());
+    @Autowired
+    private ICourseServices cs;
+
+    Course c = new Course(Long.valueOf(1), 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, new Float(0), 1, new HashSet<Registration>());
 
     @AfterEach
     public void tearDown() {
@@ -35,19 +32,15 @@ public class CourseServiceImplTest {
 
     @Test
     @Order(1)
-    public void testaddCourse(){
-        Course result=cs.addCourse(c);
+    public void testaddCourse() {
+        Course result = cs.addCourse(c);
         Assertions.assertNotNull(result);
     }
-
-
 
     @Test
     @Order(3)
-    public void testupdateCourse(){
-        Course result=cs.updateCourse(c);
+    public void testupdateCourse() {
+        Course result = cs.updateCourse(c);
         Assertions.assertNotNull(result);
     }
-
-
 }
